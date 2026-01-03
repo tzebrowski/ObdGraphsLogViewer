4df3804047d076b438c9f17d2f8f4c60a0c993ae
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2026-01-03 19:02:56 +0100

    feat: use different color schema for nav panel at analyzer view

--- src/navigation.js
+++ src/navigation.js
@@ -3,11 +3,17 @@ import { ChartManager } from './chartmanager.js';
 export const Navigation = {
   routes: {
     '#home': () => {
+      document.body.classList.remove('analyzer-active');
+      document.body.classList.add('landing-active');
+
       document.getElementById('landing-page').style.display = 'block';
       document.getElementById('analyzer-page').style.display = 'none';
       document.body.classList.add('docs-body');
     },
     '#analyzer': () => {
+      document.body.classList.add('analyzer-active');
+      document.body.classList.remove('landing-active');
+
       document.getElementById('landing-page').style.display = 'none';
       document.getElementById('analyzer-page').style.display = 'block';
       document.body.classList.remove('docs-body');
